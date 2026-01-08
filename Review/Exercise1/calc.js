543afa6c98f5b8aa9ba3216429b4bafb11a450a7
Christian Barrett <ccb9952@hotmail.com>
2026-01-08 09:30:23 -0330

    Exercise 1 Done.

--- Review/Exercise1/calc.js
+++ Review/Exercise1/calc.js
@@ -1,6 +1,25 @@
 const $ = (selector) => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    
+    $("#calculate").addEventListener("click", () => {calculate()})
+    $("#reset").addEventListener("click", () => {reset()})
 });
+
+const calculate = () => {
+    let price = parseFloat($("#price").value);
+    let rate = parseFloat($("#rate").value);
+
+    if ( isNaN(price) || price <= 0 ) {
+        alert("The price must be a valid number greater than 0");
+    } else if ( isNaN(rate) || rate <= 1 || rate >= 13 ) {
+        alert("The tax rate must be a valid number between 2 and 12");
+    } else {
+        $("#total").value = (price * ( 1 + rate / 100 )).toFixed(2);
+    }
+}
+
+const reset = () => {
+    $("#price").value = "";
+    $("#rate").value = "";
+    $("#total").value = "";
+}

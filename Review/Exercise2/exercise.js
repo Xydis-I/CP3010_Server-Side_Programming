f1d6c2434c639787b678832d3af0a788c6231ae5
Christian Barrett <ccb9952@hotmail.com>
2026-01-08 10:07:48 -0330

    Execise 2 Done.

--- Review/Exercise2/exercise.js
+++ Review/Exercise2/exercise.js
@@ -4,5 +4,51 @@ const $ = (selector) => document.querySelector(selector);
 
 
 document.addEventListener("DOMContentLoaded", () => {
-   
+   $("#login").addEventListener("click", () => {login()});
+   $("#resetButton").addEventListener("click", () => {reset()});
 });
+
+const login = () => {
+    let email = $("#email_address1").value;
+    let password = $("#password").value;
+    let robot = $("#robot").checked;
+
+    let isValidEmail = false;
+    let isValidPassword = false;
+
+    if (email == "") {
+        isValidEmail = false;
+        $("#email_address1").nextElementSibling.textContent = "Invalid email";
+    } else {
+        isValidEmail = true;
+        $("#email_address1").nextElementSibling.textContent = "*";
+    }
+
+    if (password == "") {
+        isValidPassword = false;
+        $("#password").nextElementSibling.textContent = "Invalid password";
+    } else {
+        isValidPassword = true;
+        $("#password").nextElementSibling.textContent = "*";
+    }
+
+    if (!robot) {
+        $("#robot").nextElementSibling.textContent = "Need to check box";
+    } else {
+        $("#robot").nextElementSibling.textContent = "*";
+    }
+
+    if (isValidEmail && isValidPassword && robot) {
+        document.location = "join.html";
+    }
+}
+
+const reset = () => {
+    $("#email_address1").value = "";
+    $("#password").value = "";
+    $("#robot").checked = false;
+
+    $("#email_address1").nextElementSibling.textContent = "*";
+    $("#password").nextElementSibling.textContent = "*";
+    $("#robot").nextElementSibling.textContent = "*";
+}

5dc36d11fe0c23ed2867fb7969e929c37692d37b
Xydis-I <ccb9952@hotmail.com>
2026-01-15 11:14:10 -0330

    Review Assignment Setup.

---
+++ Assignments/Review Assignment/mars_rover/rover.js
@@ -0,0 +1,29 @@
+"use strict";
+const getElement = selector => document.querySelector(selector);
+
+const domain = "https://rovers.nebulum.one/api/v1/rovers";
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    // get rover data
+
+
+
+
+
+
+
+
+    
+
+    // change event handler for Rover drop-down
+    getElement("#rover").addEventListener("change", async (evt) => {
+        
+    });
+
+    // click event handler for View button
+    getElement("#view").addEventListener("click", async () => {
+        
+    });
+});
---
+++ Assignments/Review Assignment/todo_list/todo_list.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const getElement = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+    // load <select> element
+    
+
+    // display to-do items for first user in <select> element
+    
+
+    // event handler for <select> change event
+    
+ });

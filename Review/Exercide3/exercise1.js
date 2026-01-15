18fab8a45223d84fbce8973a31d388f0f8a4f588
Xydis-I <ccb9952@hotmail.com>
2026-01-15 08:43:38 -0330

    Exercise 3 complete.

--- Review/Exercide3/exercise1.js
+++ Review/Exercide3/exercise1.js
@@ -2,14 +2,22 @@
 
 const $ = selector => document.querySelector(selector);
 
+let timer = null;
+let count = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+    $("#start").addEventListener("click", () => { start() });
+    $("#stop").addEventListener("click", () => { stop() });
 });
 
 const start = () => {
-
+    timer = setInterval(() => {
+        count++;
+        $("#count").textContent = count;
+    }, 1000);
 }
 
 const stop = () => {
-    
+    clearInterval(timer);
+    count = 0;
 }

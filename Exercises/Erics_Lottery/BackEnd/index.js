344235245e495dc10207ae19fe3363fbb23aa5be
Xydis-I <ccb9952@hotmail.com>
2026-01-22 15:26:46 -0330

    Nodemon setup with 'npm run start' & backend lottery code written.

---
+++ Exercises/Erics_Lottery/BackEnd/index.js
@@ -0,0 +1,54 @@
+import express, { request, response } from "express";
+import cors from "cors";
+
+// node index.js
+
+// nodemon
+// npm run start
+
+const prices = {
+    0: "You Lose",
+    1: "$5",
+    2: "$10",
+    3: "$15",
+    4: "$100",
+    5: "$100000"
+}
+
+let winningNumbers = [];
+
+for (let number = 0; number < 5; number++) {
+    winningNumbers.push(Math.floor(Math.random() * 14) + 1);
+}
+
+const app = express();      // create Express app object
+app.use(cors());            // add CORS middleware
+app.use(express.json());    // add JSON middleware
+
+// route that handles GET requests to the API root
+app.get("/", (request, response) => {
+    response.json({info: "An API that includes CORS headers."});
+});
+
+app.post("/api/checkticket", (request, response) => {
+    const ticket = request.body;
+    let matches = 0;
+
+    for (const number of ticket) {
+        if (winningNumbers.includes(number)) {
+            matches++;
+        }
+    }
+
+    //console.log(ticket);
+    //console.log(winningNumbers);
+    //console.log(prices[matches]);
+    
+    response.json(prices[matches]);
+});
+
+// listen for requests on port 3000
+app.listen(3000, () => {
+    console.log("API listening on port 3000.")
+});
+

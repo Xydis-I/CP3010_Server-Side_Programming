10a0dc1df3ee8e48ab1d9e57b4c08253634a9efd
C. C. Barrett <ccb9952@hotmail.com>
2026-02-02 06:12:47 -0330

    Assignment 2 Part 1 complete.

--- Assignments/Assignment_2/Eric's Weather/FrontEnd/weather.js
+++ Assignments/Assignment_2/Eric's Weather/FrontEnd/weather.js
@@ -2,13 +2,37 @@
  document.addEventListener('DOMContentLoaded', () => {
     const select = document.getElementById('citySelect');
     const display = document.getElementById('cityDisplay');
+    const report = document.getElementById('report');
 
-    select.addEventListener('change', () => {
+    select.addEventListener('change', async () => {
+        report.textContent = "";
+
         if (select.value === "") {
             display.textContent = "No city selected.";
         } else {
             const selectedText = select.options[select.selectedIndex].text;
             display.textContent = "You selected: " + selectedText;
+
+            let request = await fetch("http://localhost:3000/api/weather", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "city": selectedText })
+            });
+
+            let response = await request.json();
+
+            let description = document.createElement('p');
+            description.textContent = response.description;
+            let temperature = document.createElement('p');
+            temperature.textContent = response.temperature;
+            let windspeed = document.createElement('p');
+            windspeed.textContent = response.windspeed;
+            
+            report.appendChild(description);
+            report.appendChild(temperature);
+            report.appendChild(windspeed);
         }
     });
 });

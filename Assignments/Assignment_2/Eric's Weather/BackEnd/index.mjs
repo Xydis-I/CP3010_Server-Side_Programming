2b85ad042e96fe9804c2d43ffb7a61dad3a51c52
C. C. Barrett <ccb9952@hotmail.com>
2026-02-02 16:49:56 -0330

    Just need to add images to part 2

--- Assignments/Assignment_2/Eric's Weather/BackEnd/index.mjs
+++ Assignments/Assignment_2/Eric's Weather/BackEnd/index.mjs
@@ -1,5 +1,9 @@
 import express, { request, response } from "express";
-import cors from "cors";
+//import cors from "cors";
+import path from "path";
+import dotenv from 'dotenv';
+import { fileURLToPath } from "url";
+dotenv.config();
 
 // node index.js
 
@@ -7,20 +11,45 @@ import cors from "cors";
 // npm run start
 
 const app = express();      // create Express app object
-app.use(cors());            // add CORS middleware
+//app.use(cors());            // add CORS middleware
 app.use(express.json());    // add JSON middleware
 
-app.post("/api/weather", (request, response) => {
-    const location = request.body;
+const file = fileURLToPath(import.meta.url);
+const dir = path.dirname(file);
 
-    let description = ["Rain","Snow","Clouds","Sunshine"][Math.floor(Math.random() * 4)];
-    let temperature = `${Math.floor(Math.random() * 25) - 4}°C`
-    let windspeed = `${Math.floor(Math.random() * 8) + 16}km/h`
+app.use(express.static(path.join(dir, "FrontEnd")));
+
+app.get("/", (request, response) => {
+    response.sendFile(path.join(dir, "FrontEnd", "index.html"));
+});
+
+app.post("/api/weather", async(request, response) => {
+    const location = request.body;
+    const limit = 1;
+    const apiKey = process.env.API_SECRET_KEY;
+
+    let geoRequest = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location.city},CA&limit=${limit}&appid=${apiKey}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let geoData = await geoRequest.json();
+
+    let cityRequest = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let weatherData = await cityRequest.json();
 
     response.json({
-        "description":description, 
-        "temperature":temperature,
-        "windspeed":windspeed
+        "description":weatherData.weather[0].main, 
+        "temperature":`${(weatherData.main.temp - 273.15).toFixed(0)}°C`, 
+        "windspeed":`${weatherData.wind.speed}km/h`, 
     });
 });
 
